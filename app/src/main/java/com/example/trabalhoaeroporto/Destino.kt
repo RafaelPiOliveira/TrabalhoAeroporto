139e2df4773c0b8dfd1180e67c8c45045c8d2383
Rafael <a042611@umaia.pt>
2025-12-22 20:13:37 +0000

    eliminação e mudança de vectores

--- app/src/main/java/com/example/trabalhoaeroporto/Destino.kt
+++ app/src/main/java/com/example/trabalhoaeroporto/Destino.kt
@@ -2,7 +2,7 @@ package com.example.trabalhoaeroporto
 
 sealed class Destino(val route: String, val icon: Int, val title: String) {
     object Ecra01 : Destino(route = "ecra01", icon = R.drawable.voos, title = "Voos")
-    object Ecra02 : Destino(route = "ecra02", icon = R.drawable.favorite, title = "Favoritos")
+    object Ecra02 : Destino(route = "ecra02", icon = R.drawable.details, title = "Detalhes")
     object Ecra03 : Destino(route = "ecra03", icon = R.drawable.search, title = "Pesquisar")
     object Ecra04 : Destino(route = "ecra04", icon = R.drawable.partidas, title = "Partidas")
     object Ecra05 : Destino(route = "ecra05", icon = R.drawable.chegadas, title = "Chegadas")
